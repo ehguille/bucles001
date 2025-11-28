4ffd930994347efdda5ef75e62cbd57e3f0ba9e5
guillermogb <guillermogb@profe208b.informatica.cifpaviles.pa>
2025-11-28 14:02:38 +0100

    Bucles aninados.

--- src/bucles001/Aplicacion.java
+++ src/bucles001/Aplicacion.java
@@ -14,11 +14,15 @@ public class Aplicacion {
 			//Declaro variables
 			Scanner s=new Scanner(System.in);
 			//Muestro menú
-			System.out.println("Introduce un número del 1 al 3:");
+			System.out.println("Introduce una opción del menú:");
 			System.out.println("1) Mostrar tabla de multiplicar.");
 			System.out.println("2) División de enteros.");
 			System.out.println("3) Comprobar si un entero es primo.");
 			System.out.println("4) Factorial de un número.");
+			System.out.println("5) Comprobar contraseña.");
+			System.out.println("6) Ejemplo break.");
+			System.out.println("7) Ejemplo continue.");
+			System.out.println("8) Imprimir patrones.");
 			//Pido por teclado la opción
 			opcion=s.nextInt();
 			//Evalúo la opción
@@ -26,7 +30,7 @@ public class Aplicacion {
 			case 1:
 				System.out.println("Introduce un número:");
 				int numero=s.nextInt();
-				Ejemplos.mostrarTablaMultiplicar(numero);
+				Ejemplos.mostrarTablaMultiplicarFor(numero); 
 				break;
 			case 2:
 				System.out.println("Introduce el dividendo:");
@@ -39,19 +43,32 @@ public class Aplicacion {
 			case 3:
 				System.out.println("Introduce el número:");
 				int numeroPrimo=s.nextInt();
-				boolean esPrimo=Ejemplos.esPrimo(numeroPrimo);
+				boolean esPrimo=Ejemplos.esPrimoFor(numeroPrimo);
 				System.out.println(esPrimo);
 				break;
 			case 4:
 				System.out.println("Introduce el número:");
 				int factorial=s.nextInt();
-				System.out.println(Ejemplos.factorial(factorial));
+				System.out.println(Ejemplos.factorialFor(factorial)); 
+				break;
 			case 5:
 				Ejemplos.checkPassword();
+				break;
+			case 6:
+				Ejemplos.ejemploBreak(5);
+				break;
+			case 7:
+				Ejemplos.ejemploContinue(5);
+				break;
+			case 8:
+				//Ejemplos.imprimirPatron01(10);
+				//Ejemplos.imprimirPatron02(100);
+				Ejemplos.imprimirPatron08(6,6);
+				break;
 			default:
 				System.out.println("Opción incorrecta.");
 			}
-		} while(opcion>=1 && opcion <=5);
+		} while(opcion>=1 && opcion <=8);
 
 		
 	}
--- src/bucles001/Ejemplos.java
+++ src/bucles001/Ejemplos.java
@@ -12,6 +12,19 @@ public abstract class Ejemplos {
 		}
 	}
 	
+	public static void mostrarTablaMultiplicarFor(int numero) {
+		//situación inicial=multiplicador=0
+		//condición de ejecución=multiplicador<=10
+		//modificador de la situación multiplicador++
+		//for(situación inicial; condición; modificador){}
+		//for(int multiplicador=0;multiplicador<=10;multiplicador++) {
+		//	System.out.println(numero*multiplicador);
+		//}
+		for(int i=0;i<=10;i++) {
+			System.out.println(numero*i);
+		}
+	}
+	
 	//Método que haga una resta de números enteros utilizando sumas
 	//y solo con lo que sabéis hasta ahora.
 	//todo: 2/2=0, y debería ser 1 (revisar)
@@ -22,6 +35,15 @@ public abstract class Ejemplos {
 			resultadoResta=resultadoResta-divisor;
 			if(resultadoResta>=0)
 				cociente=cociente+1; //cociente++;
+		}
+		return cociente;
+	}
+	
+	public static int divisionEnteraFor(int dividendo, int divisor) {
+		int cociente=0;
+		for(int i=0;i<dividendo;i++) {
+			dividendo=dividendo-divisor;
+			cociente++;
 		}
 		return cociente;
 	}
@@ -38,6 +60,17 @@ public abstract class Ejemplos {
 		return esPrimo;
 	}
 	
+	public static boolean esPrimoFor(int n) {
+		boolean esPrimo=true;
+		for(int i=2;i<n;i++) {
+			if(n%i==0) {
+				esPrimo=false;
+				break;
+			}
+		}
+		return esPrimo;
+	}
+	
 	public static int calcularPago(int capitalInicial, int duracionPrestamo, int interes) {
 		return 0;
 	}
@@ -48,6 +81,14 @@ public abstract class Ejemplos {
 		while(contador<n) {
 			resultado+=contador*resultado;
 			contador++;
+		}
+		return resultado;
+	}
+	
+	public static int factorialFor(int n) {
+		int resultado=1;
+		for(int i=1;i<=n;i++) {
+			resultado=resultado*i;
 		}
 		return resultado;
 	}
@@ -66,6 +107,119 @@ public abstract class Ejemplos {
 		System.out.println("Has acertado al intento número "+contador);
 	}
 	
+	public static void ejemploBreak(int parar) {
+		int contador=0;
+		while(true) {
+			System.out.println(contador);
+			if(contador==parar)
+				break;
+			contador++;
+		}
+	}
+	
+	public static void ejemploContinue(int saltar) {
+		int contador=0;
+		while(contador<10) {
+			contador++; //contador=contador+1;
+			if(contador==saltar)
+				continue;
+			System.out.println(contador);		
+		}
+	}
+	
+	public static void imprimirPatron01(int contador) {
+		String resultado="";
+		for(int i=0;i<contador;i++) {
+			if(i%3==0)
+				resultado+="-"; //resultado=resultado+"-";
+			else
+				resultado+="*";
+		}
+		System.out.println(resultado);
+	}
+	
+	public static void imprimirPatron02(int contador) {
+		String resultado="";
+		for(int i=1;i<=contador;i++) {
+			if(i%3==0)
+				resultado+="*";
+			else if((i-1)%3==0)
+				resultado+="_"; //resultado=resultado+"-";
+			else if((i-2)%3==0)
+				resultado+="/"; //resultado=resultado+"-";
+			else
+				resultado+="??";
+		}
+		System.out.println(resultado);
+	}
+	
+	public static void imprimirPatron03(int columnas, int filas) {
+		for(int j=0;j<filas;j++) {
+			for(int i=0;i<columnas;i++) {
+				if(i==1&&j==1)
+					System.out.print("?");
+				else
+					System.out.print("*");
+			}   System.out.println("");
+		}
+	}
+	
+	public static void imprimirPatron04(int columnas, int filas) {
+		for(int j=0;j<filas;j++) {
+			for(int i=0;i<columnas;i++) {
+				if(i==j)
+					System.out.print("*");
+				else
+					System.out.print("·");
+			}   System.out.println("");
+		}
+	}
+	
 
+	public static void imprimirPatron05(int columnas, int filas) {
+		for(int j=0;j<filas;j++) {
+			for(int i=0;i<columnas;i++) {
+				if((i+j)==filas-1||i==j)
+					System.out.print("*");
+				else
+					System.out.print("·");
+			}   System.out.println("");
+		}
+	}
+
+	public static void imprimirPatron06(int columnas, int filas) {
+		for(int j=0;j<filas;j++) {
+			for(int i=0;i<columnas;i++) {
+				if((i+j)%2==0)
+					System.out.print("*");
+				else
+					System.out.print("·");
+			}   System.out.println("");
+		}
+	}
+	
+
+	public static void imprimirPatron07(int columnas, int filas) {
+		for(int j=0;j<filas;j++) {
+			for(int i=0;i<columnas;i++) {
+				if((i+j)==filas-1)
+					System.out.print("*");
+				else
+					System.out.print("·");
+			}   System.out.println("");
+		}
+	}
+
+	public static void imprimirPatron08(int columnas, int filas) {
+		for(int j=0;j<filas;j++) {
+			for(int i=0;i<columnas;i++) {
+				if(i==0||j==0||j==filas-1||i==columnas-1)
+					System.out.print("*");
+				else
+					System.out.print("·");
+			}   System.out.println("");
+		}
+	}
+		
 
 }
